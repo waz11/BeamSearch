fbdcb911c4406f88108e36da56bd7fc9b8f46d4c
Ron <ronisr@post.bgu.ac.il>
2022-04-16 21:35:40 +0300

    update

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from Graph.graph import Graph
 from Query.query import Query
 
 
-def main():
+def testBeamSearch():
     graph = Graph('./Files/graphs/src1.json')
     query = Query("list iterable node")
     searcher = BeamSearch(graph)
     result = searcher.search(query, 2)
 
     print(result)
-    result.draw()
+    # result.draw()
+
     # print(result.num_of_vertices())
     # print(result.num_of_edges())
     # searcher.model.db.print_table('src1')
     # searcher.model.db.delete_db()
+
+def main():
+    testBeamSearch()
 
 
 if __name__ == '__main__':
